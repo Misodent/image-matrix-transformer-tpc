cd9bd1e32817289fe09b7497265973919a051141
Misodent <email@malerch.com>
2026-03-24 16:59:50 +0100

    completed three of the algebraic functions

--- algebra.py
+++ algebra.py
@@ -2,20 +2,58 @@
 
 def matrix_addition(A, B):
     """Adds two matrices of the same dimensions elementwise"""
-    pass
+    
+    new_matrix = A 
+
+    for i_row, row in enumerate(new_matrix):
+        for i_column, element_A in enumerate(row):
+            element_B = B[i_row][i_column]
+            new_element = element_A + element_B
+
+            row[i_column] = new_element
+
+    return new_matrix
+
 
 def matrix_subtraction(A, B):
     """Subtracts matrix B from matrix A elementwise"""
-    pass
+
+    new_matrix = A 
+
+    for i_row, row in enumerate(new_matrix):
+        for i_column, element_A in enumerate(row):
+            element_B = B[i_row][i_column]
+            new_element = element_A - element_B
+
+            row[i_column] = new_element
+
+    return new_matrix
+
 
 def matrix_multiplication(A, B):
-    """Multiplies two matrices via cross product?"""
+    """Returns cross product of matrix A with matrix B"""
     pass
 
 def matrix_transpose(A):
-    """Transposes a matrix"""
-    pass
+    """Transposes matrix A"""
+    new_matrix = A
+
+    for i_row, row in enumerate(A):
+        for i_column, element in enumerate(row):
+            new_matrix[i_row][i_column] = A[i_column][i_row]
+            new_matrix[i_column][i_row] = A[i_row][i_column]
+
+    return new_matrix
 
 def scalar_multiplication(A, k):
     """Multiplies every value of matrix A by value k"""
-    pass
+
+    new_matrix = A
+
+    for i_row, row in enumerate(new_matrix):
+        for i_column, element in enumerate(row):
+            new_element = element * k
+            
+            row[i_column] = new_element
+            
+    return new_matrix
