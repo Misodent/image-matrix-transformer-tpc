78292b1e900bfbbe4787d927bd55dd5a9a8f8847
jasperkinschots <jasper.kinschots@gmail.com>
2026-03-30 13:29:59 +0200

    removed max's linux dotfiles

--- waybar/scripts/cpu-profile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cpu_usage=$(top -bn1 | grep 'Cpu(s)' | awk '{print int($2 + $4)}' || echo 'null')
-
-text="[ БАЛАНС: ${cpu_usage}% ]"
-
-echo "$text"
--- zsh/aliases.zsh
+++
@@ -1,3 +0,0 @@
-alias wifi-list="nmcli device wifi list"
-alias timezone-toronto="sudo timedatectl set-timezone America/Toronto"
-alias timezone-amsterdam="sudo timedatectl set-timezone Europe/Amsterdam"
