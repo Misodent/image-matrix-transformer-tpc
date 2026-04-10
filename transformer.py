baf35f9ae6b87a684d408ff6f43af31b92dac217
Misodent <email@malerch.com>
2026-04-10 15:03:26 +0200

    grayscale and transpose functions in transformer.py

--- transformer.py
+++ transformer.py
@@ -1,4 +1,6 @@
 # image transformation functions
+from algebra import (matrix_addition, matrix_subtraction, matrix_multiplication, 
+matrix_transpose, scalar_multiplication)
 
 def image_to_matrix(image):
     pass
@@ -16,10 +18,19 @@ def skew_matrix(matrix, skew_x, skew_y):
     pass
 
 def transpose_matrix(matrix):
-    pass
+    """Transpose the matrix (Swap rows and columns)"""
+    return matrix_transpose(matrix)
 
 def to_grayscale(matrix_red, matrix_green, matrix_blue):
-    pass
+    def avg_luminosity(r, g, b):
+        return 0.3 * r + 0.59 * g + 0.11 * b 
+
+    grayscale_matrix = [
+        [avg_luminosity(r, g, b) for r, g, b in zip(r_row, g_row, b_row)]
+        for r_row, g_row, b_row in zip(matrix_red, matrix_green, matrix_blue)
+    ]
+
+    return grayscale_matrix
 
 def edge_detection(matrix):
     pass
